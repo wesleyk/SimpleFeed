6deaa2577b303f4fcce241f984ec6fa9c5419d57
Wesley Kim <wesleyjkim@gmail.com>
2013-05-19 19:17:28 -0700

    Added views

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,18 @@ import play.api.mvc._
 
 object Application extends Controller {
         def index = Action {
-                Ok(views.html.index("Hello, world!"))
+                Ok(views.html.index())
+        }
+
+        def login = Action {
+                Ok(views.html.login())
+        }
+
+        def register = Action {
+                Ok(views.html.register())
+        }
+ 
+        def checkRegister = Action {
+                Ok(views.html.index())
         }
 }
